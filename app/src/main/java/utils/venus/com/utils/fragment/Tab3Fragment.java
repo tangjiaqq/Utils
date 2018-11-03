309a16678cf8f475b77fe2981cf84c9e5132bd43
tangjingxin <tangjiqq@gmail.com>
2018-11-03 16:51:29 +0800

    ...

---
+++ app/src/main/java/utils/venus/com/utils/fragment/Tab3Fragment.java
@@ -0,0 +1,41 @@
+package utils.venus.com.utils.fragment;
+
+import android.util.Log;
+import android.view.View;
+
+import com.leibown.lcfn_library.LcfnLazyLoadBaseFragment;
+
+import utils.venus.com.utils.R;
+
+public class Tab3Fragment extends LcfnLazyLoadBaseFragment {
+    private static final String TAG = "Tab3Fragment";
+
+    @Override
+    public void ButterKnifeInit(View view) {
+
+    }
+
+    @Override
+    public void unbindButterKnife() {
+
+    }
+
+    @Override
+    protected void initViews() {
+
+    }
+
+    @Override
+    protected void loadData() {
+        Log.e(TAG, "loadData: ");
+
+    }
+
+
+    @Override
+    public int getResId() {
+        return R.layout.fragment_tab1;
+    }
+
+
+}
