77848bd6d775861d36fc47857382b630b5c48402
tangjiaqq <tangjiaqq@gmail.com>
2018-11-08 20:38:47 +0800

    滚动view

--- app/src/main/java/utils/venus/com/utils/MainActivity.java
+++ app/src/main/java/utils/venus/com/utils/MainActivity.java
@@ -1,31 +1,23 @@
 package utils.venus.com.utils;
 
-import android.support.v7.app.AppCompatActivity;
+import android.content.Intent;
 import android.os.Bundle;
+import android.support.v7.app.AppCompatActivity;
 import android.support.v7.widget.LinearLayoutManager;
 import android.support.v7.widget.RecyclerView;
-import android.util.Log;
 import android.view.View;
-import android.widget.ImageView;
-import android.widget.Toast;
 
-import com.bumptech.glide.Glide;
-import com.bumptech.glide.load.engine.DiskCacheStrategy;
-import com.bumptech.glide.load.resource.drawable.GlideDrawable;
-import com.bumptech.glide.request.RequestListener;
-import com.bumptech.glide.request.animation.GlideAnimation;
-import com.bumptech.glide.request.target.SimpleTarget;
-import com.bumptech.glide.request.target.Target;
 import com.chad.library.adapter.base.BaseQuickAdapter;
 import com.chad.library.adapter.base.BaseViewHolder;
-import com.leibown.lcfn_library.glide.ShowImageUtils;
 
 import java.util.ArrayList;
 import java.util.List;
 
+import utils.venus.com.entity.MainCalss;
+
 public class MainActivity extends AppCompatActivity {
 
-    private boolean sIsScrolling;
+    private BaseQuickAdapter<MainCalss, BaseViewHolder> baseQuickAdapter;
 
     @Override
     protected void onCreate(Bundle savedInstanceState) {
@@ -34,72 +26,27 @@ public class MainActivity extends AppCompatActivity {
         RecyclerView viewById = findViewById(R.id.recyclerview);
 
         viewById.setLayoutManager(new LinearLayoutManager(this));
+        List<MainCalss> data = new ArrayList<>();
+
+        data.add(new MainCalss(ViewFlipperActivity.class, "轮播"));
 
 
-        List<String> mUrls = new ArrayList<String>();
-        mUrls.add("http://e.hiphotos.baidu.com/image/pic/item/a1ec08fa513d2697e542494057fbb2fb4316d81e.jpg");
-        mUrls.add("http://c.hiphotos.baidu.com/image/pic/item/30adcbef76094b36de8a2fe5a1cc7cd98d109d99.jpg");
-        mUrls.add("http://h.hiphotos.baidu.com/image/pic/item/7c1ed21b0ef41bd5f2c2a9e953da81cb39db3d1d.jpg");
-        mUrls.add("http://g.hiphotos.baidu.com/image/pic/item/55e736d12f2eb938d5277fd5d0628535e5dd6f4a.jpg");
-        mUrls.add("http://e.hiphotos.baidu.com/image/pic/item/4e4a20a4462309f7e41f5cfe760e0cf3d6cad6ee.jpg");
-        mUrls.add("http://b.hiphotos.baidu.com/image/pic/item/9d82d158ccbf6c81b94575cfb93eb13533fa40a2.jpg");
-        mUrls.add("http://e.hiphotos.baidu.com/image/pic/item/4bed2e738bd4b31c1badd5a685d6277f9e2ff81e.jpg");
-        mUrls.add("http://www.huabian.com/uploadfile/2014/1202/20141202025659854.jpg");
-        mUrls.add("http://www.huabian.com/uploadfile/2014/1202/20141202025700989.jpg");
-        mUrls.add("http://g.hiphotos.baidu.com/image/pic/item/0d338744ebf81a4c87a3add4d52a6059252da61e.jpg");
-        mUrls.add("http://a.hiphotos.baidu.com/image/pic/item/f2deb48f8c5494ee5080c8142ff5e0fe99257e19.jpg");
-        mUrls.add("http://f.hiphotos.baidu.com/image/pic/item/4034970a304e251f503521f5a586c9177e3e53f9.jpg");
-        mUrls.add("http://b.hiphotos.baidu.com/image/pic/item/279759ee3d6d55fbb3586c0168224f4a20a4dd7e.jpg");
-        mUrls.add("http://img2.xkhouse.com/bbs/hfhouse/data/attachment/forum/corebbs/2009-11/2009113011534566298.jpg");
-        mUrls.add("http://a.hiphotos.baidu.com/image/pic/item/e824b899a9014c087eb617650e7b02087af4f464.jpg");
-        mUrls.add("http://c.hiphotos.baidu.com/image/pic/item/9c16fdfaaf51f3de1e296fa390eef01f3b29795a.jpg");
-        mUrls.add("http://d.hiphotos.baidu.com/image/pic/item/b58f8c5494eef01f119945cbe2fe9925bc317d2a.jpg");
-        mUrls.add("http://h.hiphotos.baidu.com/image/pic/item/902397dda144ad340668b847d4a20cf430ad851e.jpg");
-        mUrls.add("http://b.hiphotos.baidu.com/image/pic/item/359b033b5bb5c9ea5c0e3c23d139b6003bf3b374.jpg");
-        mUrls.add("http://a.hiphotos.baidu.com/image/pic/item/8d5494eef01f3a292d2472199d25bc315d607c7c.jpg");
-        mUrls.add("http://b.hiphotos.baidu.com/image/pic/item/e824b899a9014c08878b2c4c0e7b02087af4f4a3.jpg");
-        mUrls.add("http://g.hiphotos.baidu.com/image/pic/item/6d81800a19d8bc3e770bd00d868ba61ea9d345f2.jpg");
-
-        viewById.setNestedScrollingEnabled(false);
-        viewById.setAdapter(new BaseQuickAdapter<String, BaseViewHolder>(R.layout.item, mUrls) {
+        baseQuickAdapter = new BaseQuickAdapter<MainCalss, BaseViewHolder>(R.layout.item, data) {
 
             @Override
-            protected void convert(BaseViewHolder helper, final String item) {
-                ImageView view = helper.getView(R.id.image);
-                Glide.with(mContext).load(item)
-                        .listener(new RequestListener<String, GlideDrawable>() {
-                            @Override
-                            public boolean onException(Exception e, String model,
-                                                       Target<GlideDrawable> target,
-                                                       boolean isFirstResource) {
-
-                                return false;
-                            }
-
-                            @Override
-                            public boolean onResourceReady(GlideDrawable resource, String model,
-                                                           Target<GlideDrawable> target,
-                                                           boolean isFromMemoryCache,
-                                                           boolean isFirstResource) {
-                                Log.e(TAG, "onResourceReady: " + item);
-
-                                return false;
-                            }
-                        }).error(R.mipmap.ic_launcher_round)
-                        .diskCacheStrategy(DiskCacheStrategy.ALL)
-                        .into(view);
-
+            protected void convert(BaseViewHolder helper, MainCalss item) {
+                helper.setText(R.id.test, item.getName());
+                helper.addOnClickListener(R.id.test);
 
             }
-        });
-
-        viewById.addOnScrollListener(new RecyclerView.OnScrollListener() {
+        };
+        viewById.setAdapter(baseQuickAdapter);
+        baseQuickAdapter.setOnItemChildClickListener(new BaseQuickAdapter.OnItemChildClickListener() {
             @Override
-            public void onScrollStateChanged(RecyclerView recyclerView, int newState) {
-                super.onScrollStateChanged(recyclerView, newState);
-
+            public void onItemChildClick(BaseQuickAdapter adapter, View view, int position) {
+                Intent intent = new Intent(MainActivity.this, baseQuickAdapter.getItem(position).getaClass());
+                startActivity(intent);
             }
-
         });
     }
 }
